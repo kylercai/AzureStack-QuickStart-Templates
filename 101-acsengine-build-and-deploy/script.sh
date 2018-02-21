766c0bae5ce0098f8fd3cc92d4ea9f6c3d0b23c4
Radhika Gupta <radhikgu@microsoft.com>
2018-02-21 11:29:39 -0800

    Update to read the API model.

--- 101-acsengine-build-and-deploy/script.sh
+++ 101-acsengine-build-and-deploy/script.sh
@@ -8,30 +8,39 @@ whoami
 
 sleep 20
 
+# Script parameters #####################################################################################################################
+API_MODEL=${1}
+
+# Get ACS-Engine repo and build #########################################################################################################
 echo "Update the system."
 sudo apt-get update -y
 
-# Installing AzureCLI #################################################################################################################
-echo "Update prerequiste for AzureCLI."
-sudo apt-get install -y libssl-dev libffi-dev python-dev build-essential -y
+echo "Install Make."
+sudo apt install make
 
-echo "Install Python 3.5"
-sudo apt-get install python3.5 -y
+echo "Clone existing repository into a new directory."
+git clone https://github.com/msazurestackworkloads/acs-engine -b acs-engine-v093
 
-echo "Install Python PIP."
-sudo apt install python-pip -y
+echo "Change directory and pull latest code."
+cd acs-engine
+git pull 
 
-echo "Upgrading Python PIP."
-pip install --upgrade pip
+echo "Install docker."
+sudo apt install docker.io -y
 
-echo "Install AzureCLI."
-sudo pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/bundled/azure-cli_bundle_0.2.10-1.tar.gz
+echo "Build developer environment."
+make devenv
 
-# Run simple command to test availablity AzureCLI
-echo "Running: az --version"
-az --version
+echo "Build the repository."
+make all
+
+echo "Write API model to a file."
+echo $API_MODEL > examples/azurestack/azurestack1.json
+echo $API_MODEL > examples/azurestack/azurestack2.json
+
+echo "Generate the template using the API model."
+./bin/acs-engine generate examples/azurestack/azurestack1.json
+
 ########################################################################################################################################
-
-
 
 echo "Completed test 101-acsengine-build-and-deploy."
