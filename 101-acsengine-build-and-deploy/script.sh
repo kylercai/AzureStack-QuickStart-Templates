ff45dd3d8c40e830d6c43cf48f1ec2aa77c0c77a
Radhika Gupta <radhikgu@microsoft.com>
2018-02-21 12:26:07 -0800

    Update to the script.

--- 101-acsengine-build-and-deploy/script.sh
+++ 101-acsengine-build-and-deploy/script.sh
@@ -10,13 +10,14 @@ sleep 20
 
 # Script parameters #####################################################################################################################
 API_MODEL=${1}
+#SUBSCRIPTION_ID=${2}
 
 # Get ACS-Engine repo and build #########################################################################################################
 echo "Update the system."
 sudo apt-get update -y
 
 echo "Install Make."
-sudo apt install make
+sudo apt install make -y
 
 echo "Clone existing repository into a new directory."
 git clone https://github.com/msazurestackworkloads/acs-engine -b acs-engine-v093
@@ -36,11 +37,13 @@ make all
 
 echo "Write API model to a file."
 echo $API_MODEL > examples/azurestack/azurestack1.json
-echo $API_MODEL > examples/azurestack/azurestack2.json
+echo $API_MODEL >> examples/azurestack/azurestack2.json
 
 echo "Generate the template using the API model."
 ./bin/acs-engine generate examples/azurestack/azurestack1.json
 
-########################################################################################################################################
+# Deploy the template using AzureCLI ###################################################################################################
 
+
+########################################################################################################################################
 echo "Completed test 101-acsengine-build-and-deploy."
