c2ba281aaccdeb0d9fd939252da701fcd04cb6c3
Radhika Gupta <radhikgu@microsoft.com>
2018-02-22 21:12:28 -0800

    Updated the script.

--- 101-acsengine-build-and-deploy/script.sh
+++ 101-acsengine-build-and-deploy/script.sh
@@ -10,9 +10,9 @@ sleep 20
 
 # Script parameters #####################################################################################################################
 BUILD_ACS_ENGINE=${1}
-API_MODEL=${2}
+API_MODEL_PATH=${2}
 echo "BUILD_ACS_ENGINE: $BUILD_ACS_ENGINE"
-echo "API_MODEL: $API_MODEL"
+echo "API_MODEL_PATH: $API_MODEL_PATH"
 
 echo "Update the system."
 sudo apt-get update -y
@@ -50,5 +50,11 @@ fi
 
 echo "Generate the template using the API model."
 ./bin/acs-engine --help
+
+echo "Download the API model"
+wget $API_MODEL_PATH --no-check-certificate
+echo "$API_MODEL_PATH" | sed "s/.*\///" >> b
+echo "File name is: $b"
+./bin/acs-engine generate $b
 
 echo "Completed test acsengine-build-and-deploy."
