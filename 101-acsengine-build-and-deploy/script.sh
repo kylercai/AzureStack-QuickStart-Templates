554afc39936452059ba1c060e30c7832a76bc534
Radhika Gupta <radhikgu@microsoft.com>
2018-02-27 16:15:49 -0800

    Updated the script.

--- 101-acsengine-build-and-deploy/script.sh
+++ 101-acsengine-build-and-deploy/script.sh
@@ -11,6 +11,13 @@ sleep 20
 # Script parameters #####################################################################################################################
 BUILD_ACS_ENGINE=${1}
 API_MODEL_PATH=${2}
+TENANT_ID=${3}
+TENANT_SUBSCRIPTION_ID=${4}
+TENANT_USERNAME=${5}
+TENANT_PASSWORD=${6}
+AZS_SA_NAME=${7}
+AZS_SA_RESOURCE_GROUP=${8}
+
 echo "BUILD_ACS_ENGINE: $BUILD_ACS_ENGINE"
 echo "API_MODEL_PATH: $API_MODEL_PATH"
 
@@ -57,14 +64,90 @@ sudo wget $API_MODEL_PATH --no-check-certificate
 echo "Inatalling pax"
 sudo apt install pax -y
 
-file_name=$(basename $API_MODEL_PATH)
-echo "File name is: $file_name"
+echo "Installing jq"
+apt install jq -y
+
+FILE_NAME=$(basename $API_MODEL_PATH)
+echo "File name is: $FILE_NAME"
+
+AZS_SA_CONTAINER_NAME=$(basename $(dirname $API_MODEL_PATH))
+echo "AzureStack container name is: $AZS_SA_CONTAINER_NAME"
+
+MASTER_DNS_PREFIX=$(jq '.properties.masterProfile.dnsPrefix' $FILE_NAME)
+TENANT_ENDPOINT=$(jq '.properties.cloudProfile.resourceManagerEndpoint' $FILE_NAME)
+STORAGE_ENDPOINT_SUFFIX=$(jq '.properties.cloudProfile.storageEndpointSuffix' $FILE_NAME)
+KEYVAULT_DNS_SUFFIX=".$(jq '.properties.cloudProfile.keyVaultDNSSuffix' $FILE_NAME | tr -d \")"
+GRAPH_ENDPOINT=$(jq '.properties.cloudProfile.graphEndpoint' $FILE_NAME)
 
 echo "Generate the template using the API model."
-sudo ./bin/acs-engine generate $file_name
+sudo ./bin/acs-engine generate $FILE_NAME
 
 echo "Accessing the generated templates."
 sudo chmod 777 -R _output/
 cd _output/
 
-echo "Completed test acsengine-build-and-deploy."
+echo "Install AzureCLI."
+echo "Update prerequiste for AzureCLI."
+sudo apt-get install -y libssl-dev libffi-dev python-dev build-essential -y
+
+echo "Install Python 3.5"
+sudo apt-get install python3.5 -y
+
+echo "Install Python PIP."
+sudo apt install python-pip -y
+
+echo "Upgrading Python PIP."
+pip install --upgrade pip
+
+echo "Install AzureCLI."
+sudo pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/bundled/azure-cli_bundle_0.2.10-1.tar.gz
+
+echo 'Import the root CA certificate to python store.'
+PYTHON_CERTIFI_LOCATION=$(python -c "import certifi; print(certifi.where())")
+sudo cat /var/lib/waagent/Certificates.pem >> $PYTHON_CERTIFI_LOCATION
+
+ENVIRONMENT_NAME=AzureStackUser
+echo 'Register to the cloud'
+az cloud register \
+  --name $ENVIRONMENT_NAME \
+  --endpoint-resource-manager $TENANT_ENDPOINT \
+  --suffix-storage-endpoint $STORAGE_ENDPOINT_SUFFIX \
+  --suffix-keyvault-dns $KEYVAULT_DNS_SUFFIX \
+  --endpoint-active-directory-graph-resource-id $GRAPH_ENDPOINT \
+  --endpoint-vm-image-alias-doc 'https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-compute/quickstart-templates/aliases.json' \
+  --profile 2017-03-09-profile
+
+az cloud set --name $ENVIRONMENT_NAME
+
+echo 'Login to the cloud'
+az login \
+  --username $TENANT_USERNAME \
+  --password $TENANT_PASSWORD \
+  --tenant $TENANT_ID
+
+az account set --subscription $TENANT_SUBSCRIPTION_ID
+
+echo 'Get connection string.'
+export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string -n $AZS_SA_NAME --resource-group $AZS_SA_RESOURCE_GROUP --query connectionString)
+export AZURE_STORAGE_ACCOUNT=$AZS_SA_NAME
+
+echo 'Uploading templates to the storage account.'
+
+az storage blob upload \
+  --container-name $AZS_SA_CONTAINER_NAME \
+  --name "$MASTER_DNS_PREFIX/azuredeploy.json" \
+  --file "/home/azureuser/acs-engine/_output/$MASTER_DNS_PREFIX/azuredeploy.json"
+
+az storage blob upload \
+  --container-name $AZS_SA_CONTAINER_NAME \
+  --name "$MASTER_DNS_PREFIX/azuredeploy.parameters.json" \
+  --file "/home/azureuser/acs-engine/_output/$MASTER_DNS_PREFIX/azuredeploy.parameters.json"
+
+
+
+
+
+
+
+
+
